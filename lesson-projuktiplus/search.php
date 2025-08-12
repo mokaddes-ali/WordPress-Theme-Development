a0417402dc00fdf0b559c6ec557bcea0e8df38d6
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2025-08-12 23:58:09 +0600

    complete search functionality in lesson theme project commit

--- lesson-projuktiplus/search.php
+++ lesson-projuktiplus/search.php
@@ -2,6 +2,82 @@
 get_header();
 ?>
 
+<section >
+    <!-- section heading -->
+    <?php get_template_part('sections/searchheading'); ?>
+
+
+  <div class="container " style="display: flex; gap: 40px;">
+    <!-- left bloge search details -->
+    <div class="blog-details" style="width: 70%;">
+    <?php
+    if (have_posts()) : ?>
+   
+    <?php
+     $count = 0;
+      while ( have_posts() ) : the_post();
+      $count++;
+      $is_even = $count % 2 === 0;
+      ?>
+  
+        <div class="single-card <?php echo $is_even ? 'reverse' : ''; ?>">
+          <div class="card-img">
+            <a href="<?php the_permalink(); ?>">
+              <?php if (has_post_thumbnail()) : ?>
+                <?php the_post_thumbnail('medium'); ?>
+              <?php endif; ?>
+            </a>
+          </div>
+          <div class="card-content">
+            <p class="post-date"><?php echo get_the_date('d M Y'); ?></p>
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <p><?php echo wp_trim_words(get_the_excerpt(), 20); ?></p>
+            <a class="read-more" href="<?php the_permalink(); ?>">
+              <?php _e('Read More', 'lessonlms'); ?>
+            </a>
+          </div>
+        </div>
+    <?php
+      endwhile;
+    ?>
+
+      <div class="pagination">
+        <?php
+        global $wp_query;
+        echo paginate_links(array(
+          'total' => $wp_query->max_num_pages,
+          'current' => max(1,get_query_var('paged')),
+        ));
+        ?>
+      </div>
+
+   <?php else : ?>
+    <div class="no-results-box">
+        <h2 class="no-results-title">
+            <?php _e( 'No Results Found', 'lessonlms' ); ?>
+        </h2>
+        <p class="no-results-text">
+            <?php _e( 'Your search for', 'lessonlms' ); ?>
+            <strong class="no-results-query">"<?php echo esc_html( get_search_query() ); ?>"</strong>
+            <?php _e( 'did not match any results.', 'lessonlms' ); ?>
+        </p>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="no-results-btn">
+            <?php _e( 'Go Back Home', 'lessonlms' ); ?>
+        </a>
+    </div>
+<?php endif; ?>
+
+
+</div>
+    
+<?php  
+get_sidebar();
+?>
+  </div>
+</section>
+
+
+
 <?php 
 get_footer();
 ?>
