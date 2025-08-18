e0950e1bbb9b3a5ec3282e9428f34d1d4d0f2123
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2025-08-18 22:58:01 +0600

    commit daynamic cta section

--- lesson-projuktiplus/functions.php
+++ lesson-projuktiplus/functions.php
@@ -303,13 +303,76 @@ function lessonlms_customize_register($wp_customize) {
     ));
 
 
+      // ======================
+  //   CTA Section
+  // ======================
+   
+  $wp_customize->add_section('cta_section', array(
+      'title' => __('CTA Settings', 'lessonlms'),
+      'priority' => 105,
+  ));
+
+
+//   CTA Title
+$wp_customize->add_setting('cta_title', array(
+    'default' => 'Take the next step toward your personal and professional goals with Lesson.',
+));
+
+$wp_customize->add_control('cta_title', array(
+    'label' => __('CTA Title', 'lessonlms'),
+    'section' => 'cta_section',
+    'type' => 'text',
+));
+
+$wp_customize->add_setting('cta_description', array(
+    'default' => 'Take the next step toward. Join now to receive personalized and more recommendations from the full Coursera catalog.',
+));
+
+$wp_customize->add_control('cta_description', array(
+    'label' => __('CTA Description', 'lessonlms'),
+    'section' => 'cta_section',
+    'type' => 'textarea',
+));
+
+// CTA Button Text
+        $wp_customize->add_setting('cta_button_text', array(
+            'default' => 'Join now',
+        ));
+
+        $wp_customize->add_control('cta_button_text', array(
+            'label' => __('CTA Button Text', 'lessonlms'),
+            'section' => 'cta_section',
+            'type' => 'text',
+        ));
+
+// CTA Button URL
+$wp_customize->add_setting('cta_button_url', array(
+    'default' => '#',
+));
+
+$wp_customize->add_control('cta_button_url', array(
+    'label' => __('CTA Button URL', 'lessonlms'),
+    'section' => 'cta_section',
+    'type' => 'url',
+));
+
+// CTA Image
+$wp_customize->add_setting('cta_image', array(
+    'default' => get_template_directory_uri() . '/assets/images/cta-right.png',
+));
+
+$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'cta_image', array(
+    'label' => __('CTA Image', 'lessonlms'),
+    'section' => 'cta_section',
+    'settings' => 'cta_image',
+)));
 
    // ======================
   //  Blog Section Start
   // ======================
   $wp_customize->add_section('blog_settings', array(
       'title'=> __('Blog Settings','lessonlms'),
-      'priority' => 90,
+      'priority' => 120,
   ));
 
   // Blog Section Title
@@ -339,7 +402,7 @@ function lessonlms_customize_register($wp_customize) {
   // ======================
   $wp_customize->add_section('footer_settings', array(
       'title'=> __('Footer Settings','lessonlms'),
-      'priority' => 120,
+      'priority' => 130,
   ));
 
   // Footer Logo
