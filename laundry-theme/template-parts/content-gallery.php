3b7136fdb4e7bffc957abee42fbd5e387660046f
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2025-08-25 22:44:07 +0600

    commit message

--- laundry-theme/template-parts/content-gallery.php
+++ laundry-theme/template-parts/content-gallery.php
@@ -4,18 +4,6 @@
     </header>
 
     <div class="entry-gallery">
-        <?php
-        $content = get_the_content();
-        preg_match_all('/<img[^>]+src="([^">]+)"/i', $content, $matches);
-
-        if (!empty($matches[1])): ?>
-            <div class="gallery">
-                <?php foreach ($matches[1] as $img_url): ?>
-                    <div class="gallery-thumbnail">
-                        <img src="<?php echo esc_url($img_url); ?>" alt="">
-                    </div>
-                <?php endforeach; ?>
-            </div>
-        <?php endif; ?>
+        
     </div>
 </article>
