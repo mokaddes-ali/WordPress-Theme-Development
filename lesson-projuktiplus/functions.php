4f5b97d078bb24c7246a1daf2f202bb67daee7a0
Mokaddes-Ali <mokaddes.ru2000@gmail.com>
2025-12-19 22:10:00 +0600

    add custoom field in register form comit

--- lesson-projuktiplus/functions.php
+++ lesson-projuktiplus/functions.php
@@ -32,3 +32,103 @@ include_once get_template_directory() ."/inc/customizer/footer.php";
 
 
 
+// Modify default register form
+add_action('register_form', 'custom_register_fields');
+function custom_register_fields() {
+    ?>
+    <style>
+        /* Hide username field */
+        #user_login {
+            display: none;
+        }
+
+        .custom-register-wrap p {
+            margin-bottom: 12px;
+        }
+
+        .custom-register-wrap input {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ddd;
+            border-radius: 6px;
+        }
+    </style>
+
+    <div class="custom-register-wrap">
+
+        <p>
+            <label>Full Name<br>
+                <input type="text" name="full_name"
+                       value="<?php echo esc_attr($_POST['full_name'] ?? ''); ?>">
+            </label>
+        </p>
+
+        <p>
+            <label>Password<br>
+                <input type="password" name="password">
+            </label>
+        </p>
+
+        <p>
+            <label>Confirm Password<br>
+                <input type="password" name="confirm_password">
+            </label>
+        </p>
+
+    </div>
+    <?php
+}
+
+add_filter('registration_errors', 'custom_register_validation', 10, 3);
+function custom_register_validation($errors, $sanitized_user_login, $user_email) {
+
+    if (empty($_POST['full_name'])) {
+        $errors->add('full_name_error', __('Full name is required.'));
+    }
+
+    if (empty($_POST['password'])) {
+        $errors->add('password_error', __('Password is required.'));
+    }
+
+    if ($_POST['password'] !== $_POST['confirm_password']) {
+        $errors->add('confirm_password_error', __('Passwords do not match.'));
+    }
+
+    return $errors;
+}
+
+
+add_filter('pre_user_login', 'generate_username_from_email');
+function generate_username_from_email($username) {
+
+    if (!empty($_POST['user_email'])) {
+        $email = sanitize_email($_POST['user_email']);
+        $username = sanitize_user(current(explode('@', $email)));
+    }
+
+    // If username exists, add random number
+    if (username_exists($username)) {
+        $username .= rand(100, 999);
+    }
+
+    return $username;
+}
+
+add_action('user_register', 'save_custom_register_data');
+function save_custom_register_data($user_id) {
+
+    // Save full name
+    if (!empty($_POST['full_name'])) {
+        update_user_meta(
+            $user_id,
+            'full_name',
+            sanitize_text_field($_POST['full_name'])
+        );
+    }
+
+    // Set password manually
+    if (!empty($_POST['password'])) {
+        wp_set_password($_POST['password'], $user_id);
+    }
+}
+
