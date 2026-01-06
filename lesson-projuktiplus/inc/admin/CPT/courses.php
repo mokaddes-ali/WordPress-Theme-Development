b8031094d90d96f0f9cea86c2afe008203eb1f6d
Mokaddes-Ali <mokaddes.ru2000@gmail.com>
2026-01-06 22:14:37 +0600

    add delet comfirm message sweetlaert commit

--- lesson-projuktiplus/inc/admin/CPT/courses.php
+++ lesson-projuktiplus/inc/admin/CPT/courses.php
@@ -444,6 +444,24 @@ jQuery(function($){
 
     // DELETE MODULE
     $(document).on('click','.delete-module',function(){
+    // alert('delete');
+    Swal.fire({
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+    Swal.fire({
+      title: "Deleted!",
+      text: "Your file has been deleted.",
+      icon: "success"
+    });
+  }
+});
         $(this).closest('.module-item').remove();
     });
 
